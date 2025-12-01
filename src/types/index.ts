de13dadf11c64b2753b60bfd3f5ca1be0efe0b16
Jose Alvarez <jose.alvarez@situm.com>
2025-12-01 14:22:41 +0100

    fix: properly export all public types

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,17 +8,22 @@
 
 export { Viewer } from "../viewer";
 export type { ViewerOptions } from "../viewer/types";
+export { SitumApiPermissionLevel } from "./auth";
 export type {
   Building,
+  BuildingListElement,
   Floor,
   Geofence,
   PathLink,
   PathNode,
   Paths,
   Poi,
+  PoiCategory,
+  PoiCreateForm,
+  PoiUpdateForm,
 } from "./cartography";
 export type { Cartesians, LatLng } from "./coordinates";
-export { ID, UUID } from "./models";
+export { CustomField, ID, Organization, UUID } from "./models";
 export { Route, RouteType } from "./route";
 export { ViewerActionParams, ViewerActionType } from "./webview_api/actions";
 export { ViewerEventPayloads, ViewerEventType } from "./webview_api/events";
