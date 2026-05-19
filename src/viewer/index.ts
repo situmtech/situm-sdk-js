533bc054c6463ae9fe9fbf1cf08b591f89edbcf7
Jose Alvarez <jose.alvarez@situm.com>
2026-05-19 13:14:29 +0200

    chore: forced commit

--- src/viewer/index.ts
+++ src/viewer/index.ts
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  *
  */
-
 import type RealtimeApi from "../domains/realtime";
 import type ReportsApi from "../domains/reports";
 import {
