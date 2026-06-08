5038214430aac14e1eb7edb5e26c1dc7ca7b0531
Jose Alvarez <jose.alvarez@situm.com>
2026-06-08 13:30:31 +0200

    chore: remove `camera.reset` message

--- src/types/webview_api/actions.ts
+++ src/types/webview_api/actions.ts
@@ -87,7 +87,6 @@ export enum ViewerActionType {
   // camera
   FOLLOW_USER = "camera.follow_user",
   CAMERA_SET = "camera.set",
-  CAMERA_RESET = "camera.reset",
 
   // session
   SESSION_RESET = "session.reset",
@@ -152,7 +151,6 @@ export type _ViewerActionParams = {
   // camera
   [ViewerActionType.FOLLOW_USER]: { value: boolean } | boolean;
   [ViewerActionType.CAMERA_SET]: Camera;
-  [ViewerActionType.CAMERA_RESET]: undefined;
 
   // session
   [ViewerActionType.SESSION_RESET]: undefined;
--- src/viewer/index.ts
+++ src/viewer/index.ts
@@ -575,15 +575,6 @@ export class Viewer {
   }
 
   /**
-   * Resets the camera to the initial view configured for the Map Viewer.
-   *
-   * Restores the initial zoom, bearing, pitch, center, and floor.
-   */
-  async resetCamera() {
-    await this.sendDataToViewer(ViewerActionType.CAMERA_RESET, undefined);
-  }
-
-  /**
    * Resets the viewer session to its initial state.
    *
    * Restores the initial camera view, clears navigation and search filters,
